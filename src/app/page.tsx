58aeacc113228ca584e7feff39fbd3cc11ccb48b
therealPatrick <malamashangala13@gmail.com>
2024-03-30 23:04:03 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { SignInButton, SignOutButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api"
+
+export default function Home() {
+  const createFile = useMutation(api.files.createFile);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <SignedIn>
+        <SignOutButton>
+          <Button>Sign Out</Button>
+        </SignOutButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <Button onClick={() => {
+        createFile({
+          name: "hello , the data is been served in the database"
+        })
+      }}>Click me!</Button>
+    </main>
+  );
+}
